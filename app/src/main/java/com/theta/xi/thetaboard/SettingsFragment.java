f5c8f35ee28adf9673ddb19466d2ef087498ceb2
Adam Seals <adam73seals@proton.me>
2025-10-21 15:36:52 -0500

    Set app to open system notification setting on clicking notification preference

--- app/src/main/java/com/theta/xi/thetaboard/SettingsFragment.java
+++ app/src/main/java/com/theta/xi/thetaboard/SettingsFragment.java
@@ -1,8 +1,13 @@
 package com.theta.xi.thetaboard;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.annotation.Nullable;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -18,5 +23,30 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey);
+        // Find the preference by its key
+        Preference notificationSettingsPref = findPreference("notifications");
+
+        if (notificationSettingsPref != null) {
+            notificationSettingsPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = new Intent();
+
+                    // Check for Android Oreo (API 26) or higher
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                        intent.putExtra(Settings.EXTRA_APP_PACKAGE, requireActivity().getPackageName());
+                    } else {
+                        // For older versions, open the general app details settings
+                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.addCategory(Intent.CATEGORY_DEFAULT);
+                        intent.setData(Uri.parse("package:" + requireActivity().getPackageName()));
+                    }
+
+                    startActivity(intent);
+                    return true;
+                }
+            });
+        }
     }
 }
