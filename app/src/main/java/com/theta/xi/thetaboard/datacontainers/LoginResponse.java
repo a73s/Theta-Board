2e7addf9c0462f2e2ae80603859a352fd02040e6
Andrew-Rollins <72422826+Andrew-Rollins@users.noreply.github.com>
2025-12-09 16:57:48 -0600

    Added backend connection, threading, and changed invite scheme.

--- app/src/main/java/com/theta/xi/thetaboard/datacontainers/LoginResponse.java
+++ app/src/main/java/com/theta/xi/thetaboard/datacontainers/LoginResponse.java
@@ -2,5 +2,4 @@ package com.theta.xi.thetaboard.datacontainers;
 
 public class LoginResponse {
     public String token;
-    public MemberInformation user;
 }
--- app/src/main/java/com/theta/xi/thetaboard/datacontainers/MemberInformation.java
+++ app/src/main/java/com/theta/xi/thetaboard/datacontainers/MemberInformation.java
@@ -2,15 +2,18 @@ package com.theta.xi.thetaboard.datacontainers;
 
 import android.provider.ContactsContract.CommonDataKinds.Email;
 
+import com.google.gson.annotations.SerializedName;
+
 public class MemberInformation {
+    @SerializedName("email")
     final public String email;
+    @SerializedName("username")
     final public String nickname;
 
     public MemberInformation() {
         this.email = "johndoe@example.com";
         this.nickname = "John Doe";
     }
-
     public MemberInformation(String email, String nickname) {
         this.email = email;
         this.nickname = nickname;
